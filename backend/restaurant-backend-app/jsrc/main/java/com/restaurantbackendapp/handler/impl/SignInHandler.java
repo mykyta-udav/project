bc57e5dc9010cf1f1d84df2b66712373dd30ba42
Andrii Romaniuk <andrewrom321@gmail.com>
2025-06-12 17:15:10 +0300

    conf + handler setup

--- backend/restaurant-backend-app/jsrc/main/java/com/restaurantbackendapp/handler/impl/SignInHandler.java
+++ backend/restaurant-backend-app/jsrc/main/java/com/restaurantbackendapp/handler/impl/SignInHandler.java
@@ -1,4 +1,14 @@
 package com.restaurantbackendapp.handler.impl;
 
-public class SignInHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.restaurantbackendapp.handler.EndpointHandler;
+
+public class SignInHandler implements EndpointHandler {
+
+    @Override
+    public APIGatewayProxyResponseEvent handle(APIGatewayProxyRequestEvent requestEvent, Context context) {
+        return null;
+    }
 }
