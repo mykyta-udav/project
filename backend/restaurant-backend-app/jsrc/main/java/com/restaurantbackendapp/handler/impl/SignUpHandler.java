a4f43927ca0ca482d1227c26d3042578563e8b23
Anastasiia Shevchuk <shevchuk2834@gmail.com>
2025-06-18 18:03:08 +0300

    Refactor repository

--- backend/restaurant-backend-app/jsrc/main/java/com/restaurantbackendapp/handler/impl/SignUpHandler.java
+++ backend/restaurant-backend-app/jsrc/main/java/com/restaurantbackendapp/handler/impl/SignUpHandler.java
@@ -1,15 +1,12 @@
 package com.restaurantbackendapp.handler.impl;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.google.gson.Gson;
 import com.restaurantbackendapp.handler.EndpointHandler;
 import com.restaurantbackendapp.dto.SignUpRequestDto;
+import com.restaurantbackendapp.repository.WaiterRepository;
 import lombok.experimental.FieldDefaults;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
@@ -17,7 +14,6 @@ import software.amazon.awssdk.services.cognitoidentityprovider.model.*;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.security.SecureRandom;
-import java.util.HashMap;
 import java.util.Map;
 
 import static lombok.AccessLevel.PRIVATE;
@@ -28,23 +24,22 @@ public class SignUpHandler implements EndpointHandler {
     CognitoIdentityProviderClient cognitoClient;
     String userPoolId;
     Gson gson;
-    AmazonDynamoDB dynamoDbClient;
+    WaiterRepository waiterRepository;
 
     static String GROUP_CUSTOMER = "Customer";
     private static final String GROUP_WAITER = "Waiter";
     static int TEMP_PASSWORD_LENGTH = 12;
-    String TABLE_NAME = System.getenv("WAITERS_TABLE");
 
     @Inject
     public SignUpHandler(
             @Named("cognitoClient") CognitoIdentityProviderClient cognitoClient,
             @Named("userPoolId") String userPoolId,
-            @Named("dynamoDbClient") AmazonDynamoDB dynamoDbClient,
+            WaiterRepository waiterRepository,
             Gson gson
     ) {
         this.cognitoClient = cognitoClient;
         this.userPoolId = userPoolId;
-        this.dynamoDbClient = dynamoDbClient;
+        this.waiterRepository = waiterRepository;
         this.gson = gson;
     }
 
@@ -73,7 +68,7 @@ public class SignUpHandler implements EndpointHandler {
             if (!listUsersResponse.users().isEmpty()) {
                 return response(400, "Email already exists");
             }
-            String roleGroup = isWaiter(email) ? GROUP_WAITER : GROUP_CUSTOMER;
+            String roleGroup = waiterRepository.existsByEmail(email) ? GROUP_WAITER : GROUP_CUSTOMER;
 
             AttributeType emailAttr = AttributeType.builder().name("email").value(email).build();
             AttributeType emailVerifiedAttr = AttributeType.builder().name("email_verified").value("true").build();
@@ -147,23 +142,5 @@ public class SignUpHandler implements EndpointHandler {
             sb.append(PASSWORD_ALLOW_BASE.charAt(rndCharAt));
         }
         return sb.toString();
-    }
-
-
-    private boolean isWaiter(String email) {
-        try {
-            Map<String, AttributeValue> key = new HashMap<>();
-            key.put("email", new AttributeValue().withS(email));
-
-            GetItemRequest getItemRequest = new GetItemRequest()
-                    .withTableName(TABLE_NAME)
-                    .withKey(key);
-
-            GetItemResult itemResult = dynamoDbClient.getItem(getItemRequest);
-
-            return itemResult.getItem() != null && !itemResult.getItem().isEmpty();
-        } catch (Exception e) {
-            return false;
-        }
     }
 }
